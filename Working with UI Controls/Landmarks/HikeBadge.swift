78066461a8677dc29fa73ee7791d27f58bb0d3e6
Jeff <jeff@cogentcoder.com>
2020-03-21 22:30:08 -0400

    Fixed typo

--- Working with UI Controls/Landmarks/HikeBadge.swift
+++ Working with UI Controls/Landmarks/HikeBadge.swift
@@ -1,7 +1,7 @@
 /*
   HikeBadge.swift
 
- A view that shows a badge for hiking.
+  A view that shows a badge for hiking.
  
   This view composes the Badge from Drawing Paths and Shapes along with some descriptive text about the hike.
  
